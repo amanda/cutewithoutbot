1f9b1ee0b68a1c240159c4acd0c57ab631d0255b
amandapickering <pickering.amanda@gmail.com>
2015-01-05 14:10:56 -0500

    fixed find_nouns

--- tellallyourfriends.py
+++ tellallyourfriends.py
@@ -11,37 +11,35 @@ OAUTH_SECRET = os.getenv('OAUTH_SECRET')
 
 twitter = Twython(API_KEY, API_SECRET, OAUTH_KEY, OAUTH_SECRET)
 
-def strip_ends(wordlist): #hacky, get rid of /r
-	new = []
-	for w in wordlist:
-		new.append(w[:-1])
+def strip_ends(wordlist): #hacky, get rid of /r from ewords file
+	new = [w[:-1].lower() for w in wordlist]
 	return new
 
 with open('endswithe.txt') as f:
-	#words = (f.read()).split('\n')
 	endswithe = f.read()
 
 with open('sowpods.txt') as f:
-	wordset = set(strip_ends((f.read()).split('\n')))
+	words = f.read()
+	wordset = set(words.split('\n'))
 
 def find_nouns(words):
 	blob = TextBlob(words)
 	tags = blob.tags
-	nouns = [w[0] for w in tags if w[1] == 'NN' or 'NNP' or 'NNS']
+	nouns = [t[0] for t in tags if t[1] == ('NN' or 'NNP' or 'NNS')]
 	return nouns
 
-nouns = set(find_nouns(endswithe)) #unicode
-e_words = set(strip_ends(endswithe.split('\n')))
+nounset = set(find_nouns(words)) #unicode
+e_wordset = set(strip_ends(endswithe.split('\n')))
 
 def generate_songlist():
 	songs = []
-	for w in e_words:
+	for w in e_wordset:
 		no_e = w[:-1]
-		if no_e in wordset and not nouns:
-			song = w.lower() + ' without the e (' + w[:-1].lower() + ' from the team)'
+		if no_e in wordset and no_e in nounset:
+			song = w.lower() + ' without the e (' + w[:-1].lower() + 's for the team)'
 			songs.append(song)
-		elif no_e in wordset and nouns:
-			song = w.lower() + ' without the e (' + w[:-1].lower() + ' for the team)'
+		elif no_e in wordset:
+			song = w.lower() + ' without the e (' + w[:-1].lower() + ' from the team)'
 			songs.append(song)
 		else:
 			pass
