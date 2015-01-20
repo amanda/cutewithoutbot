bf9657c1539abba5403f56d14555c1803ebbc1a6
amandapickering <pickering.amanda@gmail.com>
2015-01-20 12:08:29 -0500

    fixed plural nouns issue

--- tellallyourfriends.py
+++ tellallyourfriends.py
@@ -9,16 +9,7 @@ API_SECRET = os.getenv('API_SECRET')
 OAUTH_KEY = os.getenv('OAUTH_KEY')
 OAUTH_SECRET = os.getenv('OAUTH_SECRET')
 
-print API_KEY
-print API_SECRET
-print OAUTH_KEY
-print OAUTH_SECRET
-
 twitter = Twython(API_KEY, API_SECRET, OAUTH_KEY, OAUTH_SECRET)
-
-def strip_ends(wordlist): #hacky, get rid of /r from ewords file
-	new = [w[:-1].lower() for w in wordlist]
-	return new
 
 with open('endswithe.txt') as f:
 	endswithe = f.read()
@@ -27,13 +18,14 @@ with open('sowpods.txt') as f:
 	words = f.read()
 	wordset = set(words.split('\n'))
 
-def find_nouns(words):
-	blob = TextBlob(words)
-	tags = blob.tags
-	nouns = [t[0].pluralize() for t in tags if t[1] == ('NN' or 'NNP' or 'NNS')]
-	return nouns
+def strip_ends(wordlist): #hacky, get rid of /r from ewords file
+	new = [w[:-1].lower() for w in wordlist]
+	return new
 
-nounset = set(find_nouns(words)) #unicode
+blob = TextBlob(words)
+tags = blob.tags
+nounset = set([t[0] for t in tags if t[1] == ('NN' or 'NNP' or 'NNS')])
+nouns_dict = {t[0]: t[0].pluralize() for t in tags if t[1] == ('NN' or 'NNP' or 'NNS')} #{noun: plural nouns}
 e_wordset = set(strip_ends(endswithe.split('\n')))
 
 def generate_songlist():
@@ -41,18 +33,19 @@ def generate_songlist():
 	for w in e_wordset:
 		no_e = w[:-1]
 		if no_e in wordset and no_e in nounset:
-			song = w.lower() + ' without the e (' + w[:-1].lower() + ' for the team)'
+			song = w + ' without the e (' + nouns_dict[no_e] + ' for the team)'
 			songs.append(song)
 		elif no_e in wordset:
-			song = w.lower() + ' without the e (' + w[:-1].lower() + ' from the team)'
+			song = w + ' without the e (' + no_e + ' from the team)'
 			songs.append(song)
 		else:
 			pass
 	return songs
 
+songs = generate_songlist()
+
 def run():
 	try:
-		songs = generate_songlist()
 		status = choice(songs)
 		twitter.update_status(status=status)
 		print status
